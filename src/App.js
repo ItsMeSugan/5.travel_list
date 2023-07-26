7f64474ffa3b89524e4d8c93ea181109346d803a
Sugan T <t.suganeeshwaran@gmail.com>
2023-07-26 13:53:45 +0530

    lifting the state to the parent component

--- src/App.js
+++ src/App.js
@@ -7,11 +7,33 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]); // initial state as array
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+      />
       <Stats />
     </div>
   );
